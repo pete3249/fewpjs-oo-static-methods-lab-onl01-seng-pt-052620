037dce8e01093426a575456269a713dd8082a2cc
pete3249 <katelynrpeterson16@gmail.com>
2020-12-31 14:49:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9- ']+/g, '');
+  }
+
+  static titleize(string) {
+    let words = string.split(" ")
+    let modifiedWords = []
+
+    let first = words[0].charAt(0).toUpperCase() + words[0].slice(1)
+    modifiedWords.push(first)
+
+    for(let i=1; i < words.length; i++) {
+      if(!(words[i].match(/^(the|a|an|but|of|and|for|at|by|from)$/))) {
+        modifiedWords.push(words[i].charAt(0).toUpperCase() + words[i].slice(1))
+      } else {
+        modifiedWords.push(words[i])
+      }
+    }
+    return modifiedWords.join(" ")
+  }
 }
